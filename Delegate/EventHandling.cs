4c32a4fe8dc3cfdf4d9d42d6a052765368bba11f
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-05-04 13:40:13 +0530

    [Harshal]Add:Delegates Single & Multicast

---
+++ Delegate/EventHandling.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+
+    public delegate void EventHandler(string x);
+    public class EventHandling
+    {
+        public class Operations
+        {
+            public event EventHandler xyz;
+            public void Action(string a)
+            {
+                if (xyz != null)
+                {
+                    xyz(a);
+                    Console.WriteLine(a);
+                }
+                else
+                {
+                    Console.WriteLine("Not Registered");
+                }
+            }
+        }
+        private static void CatchEvent(string s)
+        {
+            Console.WriteLine("Method Calling");
+        }
+        public static void Implementation()
+        {
+            Operations o = new Operations();
+            o.Action("Event Calling");
+            Console.WriteLine();
+            o.xyz+=new EventHandler(CatchEvent);
+            o.Action("Event Called");
+        }
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static Delegate.MulticastDelegate;
+
+namespace Delegate
+{
+    public delegate int operation(int x, int y);
+    class program
+    {
+        static int Addition(int a,int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Delegate POC!");
+            //operation obj = new operation(Addition);
+            //Console.WriteLine("Addition is = {0}", obj(23, 27));
+            //Operation opration = new Operation();
+            //MulticastDelegate.ImplementDelegate();
+            EventHandling eventHandling = new EventHandling();
+            EventHandling.Implementation();
+        }
+    }
+}
